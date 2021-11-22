f4b4c0b06546a98dfc40fca9b00857284a0057fa
NoBugzzZ <739651338@qq.com>
2021-11-22 16:48:40 +0800

    chart component

---
+++ src/components/FlowChart/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { LineChart,XAxis,Tooltip,CartesianGrid,Line,YAxis } from 'recharts';
+
+export default function ({ infoWindow }) {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    if (infoWindow.visible) {
+      const content = JSON.parse(infoWindow.content)
+      const { value } = content['features']['门架历史车流量']['properties']
+      if (value) {
+        const newData=value.map(v=>{
+          return{
+            flow:v
+          }
+        })
+        setData(newData)
+        console.log(newData)
+      }
+    }
+  }, [infoWindow])
+  return (
+    <LineChart
+      width={400}
+      height={400}
+      data={data}
+      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+    >
+      <XAxis />
+      <YAxis />
+      <Tooltip />
+      <CartesianGrid stroke="#f5f5f5" />
+      <Line type="linear" dataKey="flow" stroke="#ff7300" yAxisId={0} />
+    </LineChart>
+  )
+}
